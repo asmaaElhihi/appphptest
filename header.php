ee345002ec72226e641ef496140e4c34614af9bd
ZakariaBoualaid <boulaidzac@gmail.com>
2015-04-28 00:21:42 +0100

    Add name of the developer in header.php :smile:

--- header.php
+++ header.php
@@ -1,3 +1,4 @@
 <header>
+	<strong style="font-size:40px">The developer of this project is : Asmaa Elhihi and Douaa !! (Yes she helps with saying bou3dida..)</strong>
 	This is the header
 </header>
